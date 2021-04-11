51bc70044e2d9a81c6812291c988ba29de3b9f81
mease <derek.mease@colorado.edu>
2021-04-11 17:27:13 -0600

    remove old test

--- apps/covid_tweets_web/test/covid_tweets_web/controllers/page_controller_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule CovidTweetsWeb.PageControllerTest do
-  use CovidTweetsWeb.ConnCase
-  use CovidTweetsWeb.RepoCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "CovidTweets"
-  end
-end

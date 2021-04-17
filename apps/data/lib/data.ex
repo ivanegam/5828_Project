e009edca143b5246f2a74aec3bd6d89dcdb2a63e
Ivane Gamkrelidze <71613389+ivanegam@users.noreply.github.com>
2021-04-17 16:03:17 -0600

    Add documentation throughout; remove insert_tweet_rolling/1

--- apps/data/lib/data.ex
+++ apps/data/lib/data.ex
@@ -1,65 +1,28 @@
 defmodule Data do
   @moduledoc """
-  Helper functions for inserting data.
+  Helper functions for inserting daily count, tweet, and hashtag data into the database
   """
 
-  import Ecto.Query, only: [from: 2]
-
-  # Maximum number of tweets per day to store in the database
-  @max_tweets 5
-
-  # Insert a new tweet, maintaining the maximum number of daily tweets allowed
-  # in the database. If we have the maximum number stored, and the tweet's
-  # retweet count is less than all of the existing tweets in the database,
-  # the tweet is not inserted.
-  def insert_tweet_rolling(params) do
-    # Get number of tweets in DB that occurred on the date of the tweet we want to insert.
-    tweet_date = DateTime.to_date(params.time)
-
-    query_tweet_count = from t in Data.Tweet,
-                        where: fragment("?::date", t.time) == ^tweet_date,
-                        select: count(t.id)
-
-    num_tweets = query_tweet_count |> Data.Repo.one
-
-    if num_tweets < @max_tweets do
-      # If there is space, insert the new tweet
-      insert_tweet(params)
-    else
-      # We have the maximum daily tweets. Get the tweet with the fewest number of retweets
-      query_fewest_retweets = from t in Data.Tweet,
-                              where: fragment("?::date", t.time) == ^tweet_date,
-                              order_by: [asc: :retweet_count],
-                              limit: 1
-
-      fewest_retweets =  query_fewest_retweets |> Data.Repo.one
-
-      if fewest_retweets.retweet_count < params.retweet_count do
-        # Delete the tweet with fewest retweets
-        fewest_retweets |> Data.Repo.delete
-
-        # Insert new tweet
-        insert_tweet(params)
-      else
-        {:ok, params}
-      end
-    end
-  end
-
-  # Insert a tweet.
+  @doc """
+  Inserts tweet data in the database
+  """
   def insert_tweet(params) do
     changeset = Data.Tweet.changeset(%Data.Tweet{}, params)
     Data.Repo.insert(changeset)
   end
 
-  # Insert a daily count.
+  @doc """
+  Inserts daily count data in the database
+  """
   def insert_daily_count(params) do
     changeset = Data.DailyCount.changeset(%Data.DailyCount{}, params)
     Data.Repo.insert(changeset)
   end
 
 
-  # Insert list of hashtags.
+  @doc """
+  Inserts a list of hashtags in the database
+  """
   def insert_hashtags(params) do
     changeset = Data.Hashtag.changeset(%Data.Hashtag{}, params)
     Data.Repo.insert(changeset)
--- apps/data/lib/data/tweet.ex
+++ apps/data/lib/data/tweet.ex
@@ -13,6 +13,10 @@ defmodule Data.Tweet do
     field :sentiment, :integer
   end
 
+  @doc """
+  Defines a changeset for inserting tweet data into the database,
+  conforming to pre-set validation restrictions
+  """
   def changeset(tweet, params \\ %{}) do
     tweet
     |> Ecto.Changeset.cast(params, [:time, :name, :screen_name, :profile_image_url, :text, :hashtags, :retweet_count, :label, :sentiment])
